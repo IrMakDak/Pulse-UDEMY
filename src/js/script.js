12cbdb693b5060bfe399c0bf1f80fd50fcef8b06
IR <irik.pruglo@mail.ru>
2022-07-07 00:42:27 +0200

    added tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,49 @@
+const slider = tns({
+	container: '.carousel__inner',
+	items: 1,
+	slideBy: 'page',
+	autoplay: false,
+	responsive: {
+		1035: {
+			edgePadding: 30,
+			gutter: 30,
+			items: 1
+		},
+		// 1043: {
+		// 	edgePadding: 30,
+		// 	gutter: 30,
+		// 	items: 1
+		// },
+		640: {
+			edgePadding: 30,
+			gutter: 30,
+			items: 1
+		},
+		700: {
+			edgePadding: 30,
+			items: 1,
+			gutter: 30
+		},
+		900: {
+			items: 1,
+			gutter: 30,
+			edgePadding: 30
+		}
+	}
+});
+document.querySelector('.prev').addEventListener('click', function() {
+	slider.goTo('prev');
+ });
+document.querySelector('.next').addEventListener('click', function() {
+	slider.goTo('next');
+ });
+
+// --------------------------------------------------------------------
+
 $(document).ready(function(){
-  $('.carousel__inner').slick({
-    infinite: true,
-    speed: 1200,
-    adaptiveHeight: true,
-    prevArrow: '<button type="button" class="slick-prev"><img src="../img/icons/left-line.png" alt="prev"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="../img/icons/right-line.png" alt="next"></button>',
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          dots: true,
-          arrows: false,
-        }
-      }
-    ]
-  });
+	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+		$(this)
+			.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+			.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+	});
 });
